5aca15868a8e780df4180ce2910d2f804a927ec6
Anh Tu Duong Nguyen <anhtuduong.nguyen@outlook.de>
2022-10-09 16:31:16 +0200

    add minor test cases

--- flask/test.py
+++ flask/test.py
@@ -1,27 +1,17 @@
+from turtle import home
 import unittest
 import os 
-from app import User, app, db, basedir
+from app import Admin, User, app, db, basedir, bycrypt, Product
 from flask import session
+from werkzeug.utils import secure_filename
+
 
 class BaseTest(unittest.TestCase):
-    """Helper functions
-    """    
-    def login(self, username, password):
-        return self.app.post('/login', data=dict(
-            username=username,
-            password=password
-        ), follow_redirects=True)
-        
-    def logout(self):
-        return self.app.get("/logout",
-                            follow_redirects=True)
-
     def setUp(self) -> None:
         app.config['TESTING'] = True
         app.config['WTF_CSRF_ENABLED'] = False
         app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'test.db')
         self.app = app.test_client()
-        db.drop_all()
         db.create_all()
         
     def tearDown(self) -> None:
@@ -32,26 +22,81 @@ class BaseTest(unittest.TestCase):
         res = self.app.get("/")
         self.assertEqual(res.status_code, 200)
         
-    def test_users_login_logout(self):
+    def test_home_redirect(self):
+        res = self.app.get("/home")
+        self.assertEqual(res.status_code, 302)   
+        
+            
+    def test_home(self):
         u = User(
             id = 1,
-            name = "tuan",
-            surname = "tran", 
-            username="gaucan", 
-            password="1111")
-        db.session.add(u)
+            name = "maxim",
+            surname= "gomez",
+            username="rehabwert22",
+            password="1111"
+        )
+        res = self.app.post("/login", data=dict(username=u.username,
+                                          password=u.password))
+        self.assertEqual(res.status_code, 200)
+        
+    def test_log_out_redirect(self):
+        res = self.app.get("/logout")
+        self.assertEqual(res.status_code, 302)
+        
+    def test_logout(self):
+        res = self.app.post("/login", data=dict(
+            username="1111",
+            password="1111",
+            follow_redirects=True
+        ))
+        self.assertEqual(res.status_code, 200)
+        res = self.app.get("/logout")
+        self.assertEqual(res.status_code, 302)
+        
+    def test_admin_set_up(self):
+        a = Admin(
+            id=1,
+            name="maxim",
+            sur_name="gomez",
+            admin_name="rehabwert22",
+            password="1111"
+        )
+        db.session.add(a)
         db.session.commit()
         
-        rv = self.login(
-            "gauncan",
-            "1111", 
-        )        
-        self.assertEqual(rv.status_code, 200)
-        rv = self.logout()
-        self.assertEqual(rv.status_code, 200)
+        
+    def test_admin_home(self):
+        res = self.app.get("/admin")
+        self.assertEqual(res.status_code, 200)
+        
+    def test_admin_set_up_form(self):
+        res = self.app.get("/admin/setup")
+        self.assertEqual(res.status_code, 200)
+        
+    def test_admin_log_in(self):
+        a = Admin(
+            id=1,
+            name="maxim",
+            sur_name="gomez",
+            admin_name="rehabwert22",
+            password="1111"
+        )
         
         
+    def test_account(self):
+        res = self.app.get("/account")
+        self.assertEqual(res.status_code, 302)
+    
+    def test_register(self):
+        res = self.app.get("/register")
+        self.assertEqual(res.status_code, 200)    
         
         
+    def test_addproduct_form(self):
+        res = self.app.get("/addproduct")
+        self.assertEqual(res.status_code, 200)    
+        
+    
+    
 if __name__ == "__main__":
     unittest.main(  )
